bf54908c518cd1c02959231c0f9a5ff959557bff
Anton O <mavarazy@gmail.com>
2015-05-08 21:43:50 +0300

    Some normalization

--- src/main/java/com/clemble/casino/server/payment/repository/ServerAccountService.java
+++ src/main/java/com/clemble/casino/server/payment/repository/ServerAccountService.java
@@ -3,8 +3,9 @@ package com.clemble.casino.server.payment.repository;
 import com.clemble.casino.payment.PaymentTransaction;
 import com.clemble.casino.payment.PendingTransaction;
 import com.clemble.casino.payment.PlayerAccount;
+import com.clemble.casino.server.ServerService;
 
-public interface ServerAccountService {
+public interface ServerAccountService extends ServerService {
 
     public PlayerAccount findOne(String player);
 
