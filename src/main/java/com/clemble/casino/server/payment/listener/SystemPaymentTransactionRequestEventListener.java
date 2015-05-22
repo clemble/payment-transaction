108ee4e34989504e2806ee2ebd3a04d99d967a50
Anton O <mavarazy@gmail.com>
2015-05-22 06:45:09 +0300

    Update error handling

--- src/main/java/com/clemble/casino/server/payment/listener/SystemPaymentTransactionRequestEventListener.java
+++ src/main/java/com/clemble/casino/server/payment/listener/SystemPaymentTransactionRequestEventListener.java
@@ -38,7 +38,7 @@ public class SystemPaymentTransactionRequestEventListener implements SystemEvent
         LOG.debug("{} start", paymentTransaction.getTransactionKey());
         // Step 1. Sanity check
         if (paymentTransaction == null)
-            throw ClembleException.fromError(ClembleErrorCode.PaymentTransactionEmpty);
+            throw ClembleException.withServerError(ClembleErrorCode.PaymentTransactionEmpty);
         validationService.validate(paymentTransaction);
         // Step 2. Processing payment transactions
         accountTemplate.process(paymentTransaction);
